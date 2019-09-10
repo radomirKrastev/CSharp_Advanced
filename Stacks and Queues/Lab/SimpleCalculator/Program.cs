f17f094d8facc8fb000d42fdd5e5566848ed46ef
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-10 09:22:10 +0300

    stack problem

---
+++ Stacks and Queues/Lab/SimpleCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            var stack = new Stack<string>(input.Reverse());
+            var sum = int.Parse(stack.Pop());
+
+            while (stack.Any())
+            {
+                var nextSymbol = stack.Pop();
+                if (nextSymbol == "+")
+                {
+                    sum += int.Parse(stack.Pop());
+                }
+                else if (nextSymbol == "-")
+                {
+                    sum -= int.Parse(stack.Pop());
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
