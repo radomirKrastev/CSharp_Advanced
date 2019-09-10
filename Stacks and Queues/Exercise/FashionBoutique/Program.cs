36995592f0e422065d03e0ed02801882d820cdb8
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-10 17:06:10 +0300

    stacks and queues problem

---
+++ Stacks and Queues/Exercise/FashionBoutique/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var clothesValues = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var rackInitialCapacity = int.Parse(Console.ReadLine());
+            var clothes = new Stack<int>(clothesValues);
+            var racksUsed = 1;
+            var rackCurrentCapacity = rackInitialCapacity;
+
+            while (clothes.Any())
+            {
+                var cloth = clothes.Pop();
+
+                if (rackCurrentCapacity >=cloth)
+                {
+                    rackCurrentCapacity -= cloth;
+                }
+                else
+                {
+                    rackCurrentCapacity = rackInitialCapacity;
+                    racksUsed++;
+                    clothes.Push(cloth);
+                }
+            }
+
+            Console.WriteLine(racksUsed);
+        }
+    }
+}
