27cc73d6aa3cb5f1f44cf32348cef06311a92c83
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-10 10:29:37 +0300

    queue problem

---
+++ Stacks and Queues/Lab/Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Supermarket
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var command = Console.ReadLine();
+            var supermarketQueue = new Queue<string>();
+
+            while (command != "End")
+            {
+                if (command == "Paid")
+                {
+                    while (supermarketQueue.Any())
+                    {
+                        Console.WriteLine(supermarketQueue.Dequeue());
+                    }
+                }
+                else
+                {
+                    supermarketQueue.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(supermarketQueue.Count+" people remaining.");
+        }
+    }
+}
