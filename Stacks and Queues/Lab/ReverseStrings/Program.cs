99f50f5029a8910b5055686cd40bda09258a13cd
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-10 08:38:37 +0300

    stack problem

---
+++ Stacks and Queues/Lab/ReverseStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseStrings
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<string>();
+
+            foreach (var ch in input)
+            {
+                stack.Push(ch.ToString());
+            }
+            
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
---
+++ Stacks and Queues/Lab/StackSum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace StackSum
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse);
+            var stack = new Stack<int>(input);
+            var command = Console.ReadLine().Split();
+            while (command.Count() > 1)
+            {
+                if (command.Count() == 3)
+                {
+                    var numberOne = int.Parse(command[1]);
+                    var numberTwo = int.Parse(command[2]);
+                    stack.Push(numberOne);
+                    stack.Push(numberTwo);
+                }
+                else
+                {
+                    var count = int.Parse(command[1]);
+
+                    if (count == stack.Count)
+                    {
+                        stack.Clear();
+                    }
+                    else if(count<stack.Count)
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }                    
+                }
+
+                command = Console.ReadLine().Split();
+            }
+
+            int sum = 0;
+            while (stack.Count > 0)
+            {
+                sum+=stack.Pop();
+            }
+
+            Console.WriteLine("Sum: "+sum);
+        }
+    }
+}
