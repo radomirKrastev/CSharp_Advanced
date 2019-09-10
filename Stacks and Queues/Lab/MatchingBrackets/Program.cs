1ad489a2d08fbcde5d8b55068b91a25337e0d953
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-10 10:17:23 +0300

    stack problem

---
+++ Stacks and Queues/Lab/MatchingBrackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var openBrackets = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    openBrackets.Push(i);
+                }
+                else if(input[i] == ')')
+                {
+                    var start = openBrackets.Pop();
+                    var length = i - start +1;
+                    var expression = input.Substring(start, length);
+                    Console.WriteLine(expression);
+                }
+            }          
+        }
+    }
+}
