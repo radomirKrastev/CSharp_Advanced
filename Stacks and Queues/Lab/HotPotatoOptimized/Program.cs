4b96c4add38777eff99d9cde0753db13a2b1d4e9
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-10 11:35:56 +0300

    hot potato problem another approach

---
+++ Stacks and Queues/Lab/HotPotatoOptimized/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotatoOptimized
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            var hotNumber = int.Parse(Console.ReadLine());
+            var children = new Queue<string>(input);
+            var counter = 1;
+
+            while (children.Count > 1)
+            {
+                var child = children.Dequeue();
+
+                if (counter % hotNumber == 0)
+                {
+                    Console.WriteLine("Removed " + child);
+                }
+                else
+                {
+                    children.Enqueue(child);
+                }
+
+                counter++;
+            }
+
+            Console.WriteLine("Last is " + children.Dequeue());
+        }
+    }
+}
