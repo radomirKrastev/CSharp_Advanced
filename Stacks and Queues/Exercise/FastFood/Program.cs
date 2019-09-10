092ac3c1f6850148760e6c59f6b40c7d402412bc
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-10 16:10:15 +0300

    stacks and queues problem

---
+++ Stacks and Queues/Exercise/FastFood/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FastFood
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var quantityPrepared = int.Parse(Console.ReadLine());
+            var orders = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Console.WriteLine(orders.Max());
+            var queue = new Queue<int>(orders);
+            bool ordersComplete = false;
+
+            while (queue.Any())
+            {
+                var currentOrder = queue.Dequeue();
+                if (quantityPrepared >= currentOrder)
+                {
+                    quantityPrepared -= currentOrder;
+                    ordersComplete = true;
+                }
+                else
+                {
+                    string result = "Orders left: "+ currentOrder.ToString()+" ";
+
+                    if (queue.Any())
+                    {
+                        result+=string.Join(" ",queue.ToArray()).ToString();
+                    }
+
+                    Console.WriteLine(result);
+                    ordersComplete = false;
+                    break;
+                }
+            }
+
+            if (ordersComplete)
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
