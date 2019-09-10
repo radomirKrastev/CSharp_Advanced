94853a10633cc7ebcf2783addb4092fa3b90b076
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-10 12:08:34 +0300

    queue problem

---
+++ Stacks and Queues/Lab/TrafficJam/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficJam
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var crossroadLimit = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>();
+            var command = Console.ReadLine();
+            var carsPassed = new StringBuilder();
+            var counter = 0;
+
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    if (crossroadLimit <= queue.Count)
+                    {
+                        for (int i = 0; i < crossroadLimit; i++)
+                        {
+                            counter++;
+                            carsPassed.AppendLine(queue.Dequeue() + " passed!");
+                        }
+                    }
+                    else
+                    {
+                        while (queue.Any())
+                        {
+                            counter++;
+                            carsPassed.AppendLine(queue.Dequeue() + " passed!");
+                        }
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.Write(carsPassed);
+            Console.Write(counter+" cars passed the crossroads.");
+        }
+    }
+}
