ef428c50aebd76555f0581dae91f4b35716ece1b
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-11 22:27:21 +0300

    stacks and queues problem

---
+++ Stacks and Queues/Exercise/Crossroads/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Crossroads
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var greenLightDuration = int.Parse(Console.ReadLine());
+            var freeWindowDuration = int.Parse(Console.ReadLine());
+            var command = Console.ReadLine();
+            var crossroadQueue = new Queue<string>();
+            var carsThatPassed = 0;
+
+            while (command != "END")
+            {
+                if (command == "green")
+                {
+                    var greenLightLeft = greenLightDuration;
+                    var passingDuration = greenLightDuration+freeWindowDuration; 
+
+                    while (crossroadQueue.Any())
+                    {
+                        var carPassing = crossroadQueue.Dequeue();
+                        var carPassingTime = carPassing.Length;
+                        
+                        if (greenLightLeft>0)
+                        {
+                            if (carPassingTime <= passingDuration)
+                            {
+                                carsThatPassed++;
+                                passingDuration -= carPassingTime;
+                                greenLightLeft -= carPassingTime;
+                                crossroadQueue.Enqueue(carPassing);
+                            }
+                            else
+                            {
+                                var difference = carPassingTime - passingDuration;
+                                var hitPart = carPassing[carPassing.Length - difference];
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine(carPassing + " was hit at " + hitPart + ".");
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    crossroadQueue.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine(carsThatPassed+ " total cars passed the crossroads.");
+        }
+    }
+}
---
+++ Stacks and Queues/Exercise/SimpleTextEditor/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTextEditor
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var commandsCount = int.Parse(Console.ReadLine());
+            var text = string.Empty;
+            var lastModification = new Stack<string>();
+
+            for (int i = 0; i < commandsCount; i++)
+            {
+                var command = Console.ReadLine().Split();
+                if (command[0] == "1")
+                {
+                    lastModification.Push(text.ToString());
+                    text += command[1];
+                }
+                else if (command[0] == "2")
+                {
+                    if (text.Length > 0)
+                    {
+                        lastModification.Push(text.ToString());
+                        var symbolsToRemove = int.Parse(command[1]);
+                        if (symbolsToRemove >= text.Length)
+                        {
+                            text = string.Empty;
+                        }
+                        else
+                        {
+                            text.Remove(text.Length - symbolsToRemove - 1, symbolsToRemove);
+                        }
+                    }
+                }
+                else if (command[0] == "3")
+                {
+                    var position = int.Parse(command[1]) - 1;
+
+                    if (position >= 0 && position < text.Length)
+                    {
+                        Console.WriteLine(text[position]);
+                    }
+                }
+                else
+                {
+                    text = lastModification.Pop();
+                }
+            }
+        }
+    }
+}
