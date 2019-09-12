7ee94c792848ba79daa6a33952d97a9b45092e63
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-12 15:26:19 +0300

    multidimension arrays problems

---
+++ Multidimensional arrays/Lab/PrimaryDiagonal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var size = int.Parse(Console.ReadLine());
+            var matrix = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                var currentLine = Console.ReadLine()
+                    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = currentLine[col];
+                }
+            }
+
+            var currentRow = 0;
+            var currentCol = 0;
+            var sum = 0;
+
+            while(currentRow<size && currentCol < size)
+            {
+                sum += matrix[currentRow++, currentCol++];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
