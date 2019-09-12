d52390990ee13123f08f9d0dbdd1b974295488d7
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-12 16:08:13 +0300

    multidimension arrays problems

---
+++ Multidimensional arrays/Lab/SquareWithMaximumSum/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace SquareWithMaximumSum
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var dimensions = Console.ReadLine()
+                .Split(new string[] { " ", "," }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = dimensions[0];
+            var cols = dimensions[1];
+            var matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentLine = Console.ReadLine()
+                .Split(new string[] { " ", "," }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentLine[col];
+                }
+            }
+            
+            var maxSum = int.MinValue;
+            var maxRowIndex = 0;
+            var maxColIndex = 0;
+
+            for (int row = 0; row < rows-1; row++)
+            {
+                for (int col = 0; col < cols-1; col++)
+                {
+                    var currentSum = matrix[row, col]
+                        + matrix[row, col + 1]
+                        + matrix[row + 1, col]
+                        + matrix[row + 1, col + 1];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxRowIndex = row;
+                        maxColIndex = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{matrix[maxRowIndex,maxColIndex]} {matrix[maxRowIndex,maxColIndex+1]}");
+            Console.WriteLine($"{matrix[maxRowIndex+1,maxColIndex]} {matrix[maxRowIndex+1,maxColIndex+1]}");
+            Console.WriteLine(maxSum);
+        }
+    }
+}
