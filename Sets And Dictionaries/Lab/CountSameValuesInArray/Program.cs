1ff5d77f5c9c96031e1dc701d3435a9b016d77bb
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-16 17:16:44 +0300

    dictionaries

---
+++ Sets And Dictionaries/Lab/CountSameValuesInArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSameValuesInArray
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+            var numbersCount = new Dictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!numbersCount.ContainsKey(number))
+                {
+                    numbersCount.Add(number, 0);
+                }
+
+                numbersCount[number]++;
+            }
+
+            foreach (var number in numbersCount)
+            {
+                Console.WriteLine(number.Key+" - "+number.Value+" times");
+            }
+        }
+    }
+}
