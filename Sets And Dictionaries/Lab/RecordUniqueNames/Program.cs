638c129795a320a38315fec432dbc12aa3b6d027
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-17 09:03:59 +0300

    sets

---
+++ Sets And Dictionaries/Lab/RecordUniqueNames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordUniqueNames
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var namesCount = int.Parse(Console.ReadLine());
+            var names = new HashSet<string>();
+
+            for (int i = 0; i < namesCount; i++)
+            {
+                var name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
