883fbe23c9c7ccc4b4f1ecc495403d52717afd9a
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-17 13:44:43 +0300

    sets and dictionaries

---
+++ Sets And Dictionaries/Exercise/CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CountSymbols
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var symbolCount = new Dictionary<char, int>();
+            var text = Console.ReadLine();
+
+            foreach (var ch in text)
+            {
+                if (!symbolCount.ContainsKey(ch))
+                {
+                    symbolCount.Add(ch, 0);
+                }
+
+                symbolCount[ch]++;
+            }
+
+            foreach (var symbol in symbolCount.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
---
+++ Sets And Dictionaries/Exercise/EvenTimes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var inputCount = int.Parse(Console.ReadLine());
+            var numbersCount = new Dictionary<int, int>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+
+                if (!numbersCount.ContainsKey(number))
+                {
+                    numbersCount.Add(number, 0);
+                }
+
+                numbersCount[number]++;
+            }
+
+            foreach (var number in numbersCount.Where(x=>x.Value%2==0))
+            {
+                Console.WriteLine(number.Key);
+            }
+        }
+    }
+}
