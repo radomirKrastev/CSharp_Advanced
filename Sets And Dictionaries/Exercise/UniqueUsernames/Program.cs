095adec932a32330ebb55cc7bfa1dedf6c10fb5f
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-17 10:12:32 +0300

    sets

---
+++ Sets And Dictionaries/Exercise/UniqueUsernames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var inputsCount = int.Parse(Console.ReadLine());
+            var uniqueUsernames = new HashSet<string>();
+
+            for (int i = 0; i < inputsCount; i++)
+            {
+                var username = Console.ReadLine();
+
+                uniqueUsernames.Add(username);
+            }
+
+            foreach (var username in uniqueUsernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
