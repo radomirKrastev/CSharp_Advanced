a93d51a8f63e368180ede588acc0022070083ab0
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-17 12:27:10 +0300

    sets

---
+++ Sets And Dictionaries/Exercise/PeriodicTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeriodicTable
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var chemicalsCount = int.Parse(Console.ReadLine());
+            var chemicals = new SortedSet<string>();
+
+            for (int i = 0; i < chemicalsCount; i++)
+            {
+                var elements = Console.ReadLine().Split();
+
+                foreach (var element in elements)
+                {
+                    chemicals.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicals));
+        }
+    }
+}
