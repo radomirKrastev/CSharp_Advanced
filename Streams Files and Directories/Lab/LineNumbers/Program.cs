00c3f81bd0c265f970761930395b972ac03f6707
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-19 13:16:48 +0300

    using Stream

---
+++ Streams Files and Directories/Lab/LineNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using(var reader = new StreamReader(@"02. Line Numbers\input.txt"))
+            {
+                using(var writer = new StreamWriter("output.txt"))
+                {
+                    var line = reader.ReadLine();
+                    var currentLine = 1;
+
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{currentLine++}. {line}");
+                        line = reader.ReadLine();
+                    }                    
+                }
+            } 
+        }
+    }
+}
