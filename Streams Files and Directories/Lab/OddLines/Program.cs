c6ddb844b504128fca20c5aeadda8cee23a860b1
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-19 13:05:22 +0300

    using Stream

---
+++ Streams Files and Directories/Lab/OddLines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using (var reader = new StreamReader("input.txt"))
+            {
+                var counter = 0;
+                var line = reader.ReadLine();
+
+                using (var writer = new StreamWriter("output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (counter++ % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        line = reader.ReadLine();
+                    }                    
+                }                
+            }
+        }
+    }
+}
