1f7ee12c8195288dc591609564ed4179528dbb42
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-19 14:25:48 +0300

    using Stream

---
+++ Streams Files and Directories/Lab/MergeFiles/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace MergeFiles
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using(var readerOne = new StreamReader("FileOne.txt"))
+            {
+                var lineFirstFile = readerOne.ReadLine();
+
+                using (var readerTwo = new StreamReader("FileTwo.txt"))
+                {
+                    using (var writer = new StreamWriter("Output.txt"))
+                    {
+                        var lineSecondFile = readerTwo.ReadLine();
+
+                        while (true)
+                        {
+                            if(lineFirstFile == null && lineSecondFile == null)
+                            {
+                                break;
+                            }
+
+                            if (lineFirstFile != null)
+                            {
+                                writer.WriteLine(lineFirstFile);
+                                lineFirstFile = readerOne.ReadLine();
+                            }
+                            if (lineSecondFile != null)
+                            {
+                                writer.WriteLine(lineSecondFile);
+                                lineSecondFile = readerTwo.ReadLine();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
