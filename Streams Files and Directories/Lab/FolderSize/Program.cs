33edd4aeedd14dc72f01363c5de03a258c5b7ad1
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-19 16:29:31 +0300

    using Directory & FileInfo

--- Streams Files and Directories/Lab/FolderSize/Program.cs
+++ Streams Files and Directories/Lab/FolderSize/Program.cs
@@ -8,16 +8,16 @@ namespace FolderSize
         public static void Main()
         {
             string [] files = Directory.GetFiles("TestFolder");
-            double sum = 0;
+            double filesTotalSum = 0;
 
             foreach (var file in files)
             {
                 FileInfo fileInfo = new FileInfo(file);
-                sum += fileInfo.Length;
+                filesTotalSum += fileInfo.Length;
             }
 
-            sum = sum / 1024 / 1024;
-            File.WriteAllText("output.txt", sum.ToString());
+            filesTotalSum = filesTotalSum / 1024 / 1024;
+            File.WriteAllText("output.txt",filesTotalSum.ToString());
         }
     }
 }
