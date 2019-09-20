5fb4aa6375d5f1f780bcc049a83ef4e27c8f0fc5
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-20 18:31:24 +0300

    Functional programming

---
+++ Functional Programming/Lab/SumNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SumNumbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+               .Split(", ")
+               .Select(int.Parse)
+               .ToList();
+
+            Action<List<int>> print = PrintCountAndSum;
+            print(numbers);
+            
+        }
+
+        public static void PrintCountAndSum(List<int> list)
+        {
+            Console.WriteLine(list.Count());
+            Console.WriteLine(list.Sum());
+        }
+    }
+}
