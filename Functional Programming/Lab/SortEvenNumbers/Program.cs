48ff97362667dc1bc34dc66ee90a53f20e2c2f21
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-20 18:21:28 +0300

    Functional programming

---
+++ Functional Programming/Lab/SortEvenNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SortEvenNumbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers= Console.ReadLine()
+               .Split(", ")
+               .Select(int.Parse)
+               .Where(x => x % 2 == 0)
+               .ToList();
+
+            Func<List<int>, List<int>> sortAndPrint = SortAndPrint;
+            sortAndPrint(numbers);
+        }
+
+        public static List<int> SortAndPrint(List<int> list)
+        {
+            list.Sort();
+            Console.WriteLine(string.Join(", ",list));
+            return list;
+        }
+    }
+}
