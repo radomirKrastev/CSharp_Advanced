8142464d7cbd1d9e3430ede2054a3b56ccda0308
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-21 10:18:18 +0300

    Functional programming

---
+++ Functional Programming/Lab/addVAT/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace addVAT
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var prices = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Action<double[]> pricesAction = AddVatPrintResult;
+            pricesAction(prices);
+        }
+
+        public static void AddVatPrintResult(double[] prices)
+        {
+            for (int i = 0; i < prices.Length; i++)
+            {
+                prices[i] *= 1.2;
+            }
+
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:F2}");
+            }
+        }
+    }
+}
