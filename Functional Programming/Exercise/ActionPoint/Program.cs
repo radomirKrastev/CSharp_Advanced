b28713e21234d1223a6ffbf649f2f3013917cd29
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-21 22:54:53 +0300

    functional programming

---
+++ Functional Programming/Exercise/ActionPoint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPoint
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var strings = Console.ReadLine().Split();
+            Action<string> print;
+            print = s => Console.WriteLine(s);
+            strings.ToList().ForEach(print);
+        }
+    }
+}
