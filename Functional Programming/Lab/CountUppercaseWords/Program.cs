b8f5ec22eb0e80c09c1c4db5274186307522e83f
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-21 09:33:58 +0300

    Functional programming

---
+++ Functional Programming/Lab/CountUppercaseWords/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.ReadLine()
+                .Split(new string[] { " " },StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => char.IsUpper(x[0]))
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
