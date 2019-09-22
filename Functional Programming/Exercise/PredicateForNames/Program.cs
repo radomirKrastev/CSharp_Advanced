628c7b61082c0e8ad2e9cfd0fc09649342f96236
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-22 14:16:59 +0300

    functional programming

---
+++ Functional Programming/Exercise/PredicateForNames/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var length = int.Parse(Console.ReadLine());
+            Console.ReadLine().Split().Where(x => x.Length <= length).ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
