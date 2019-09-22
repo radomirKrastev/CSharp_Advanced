4ce154151067d28a2bc14f75f19b6ff5c99f88fd
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-22 14:51:44 +0300

    functional programming

---
+++ Functional Programming/Exercise/CustomComparator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int, int, int> sortFunc = (a, b) => a.CompareTo(b);
+
+            var evenNumbers = numbers.Where(x => x % 2 == 0).ToArray();
+            var oddNumbers = numbers.Where(x => x % 2 != 0).ToArray();
+
+            Array.Sort(evenNumbers, new Comparison<int>(sortFunc));
+            Array.Sort(oddNumbers, new Comparison<int>(sortFunc));
+
+            Action<int[], int []> print = (x,y) => Console.WriteLine(string.Join(" ",x)+" "+string.Join(" ",y));
+            print(evenNumbers,oddNumbers);
+        }
+    }
+}
