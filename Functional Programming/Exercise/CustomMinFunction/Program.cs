2c509391ab4af91cbb38452b5a7e2ad7cb76f149
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-22 10:47:20 +0300

    functional programming

---
+++ Functional Programming/Exercise/CustomMinFunction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CustomMinFunction
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            Func<List<int>,int> min = Min;
+            Console.WriteLine(min(numbers));
+        }
+
+        public static int Min(List<int> numbers)
+        {
+            var minNumber = int.MaxValue;
+            foreach (var number in numbers)
+            {
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+
+            return minNumber;
+        }
+    }   
+}
