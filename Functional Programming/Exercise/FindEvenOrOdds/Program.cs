e66a45f68bae1c1c75518ce5233c24e560ee3aa2
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-22 11:22:41 +0300

    functional programming

---
+++ Functional Programming/Exercise/FindEvenOrOdds/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FindEvenOrOdds
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var border = Console.ReadLine().Split();
+
+            var start = int.Parse(border[0]);
+            var end = int.Parse(border[1]);
+            var numbers = new List<int>();
+
+            for (int i = start; i <= end; i++)
+            {
+                numbers.Add(i);
+            }
+
+            var condition = Console.ReadLine();
+            Func<int, bool> filter = x => condition == "odd" ? x % 2 != 0 : x % 2 == 0;
+            Console.WriteLine(string.Join(" ",numbers.Where(filter)));
+        }
+    }
+}
