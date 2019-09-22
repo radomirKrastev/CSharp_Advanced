443555e2dbdfb1be9bf9d306b9f917282610937b
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-22 13:50:52 +0300

    functional programming

---
+++ Functional Programming/Exercise/ReverseAndExclude/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ReverseAndExclude
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).Reverse().ToList();
+            var key = int.Parse(Console.ReadLine());
+            Func<int, bool> excludeAndReverse;
+            excludeAndReverse = x => x % key != 0;
+            Console.WriteLine(string.Join(" ",numbers.Where(excludeAndReverse)));
+        }
+    }
+}
