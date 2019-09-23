b98af09e92377c6b4b42c0b7ab1fba5f4ba753a8
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-23 18:59:34 +0300

    optimize

--- Functional Programming/Exercise/PredicateParty/Program.cs
+++ Functional Programming/Exercise/PredicateParty/Program.cs
@@ -13,92 +13,66 @@ namespace PredicateParty
                 .ToList();
 
             var command = Console.ReadLine();
-            Func<string, List<string>, List<string>> guestsProcessing = DoubleOrRemove;
+            Func<string, string, bool> guestsPredicate;            
 
             while (command != "Party!")
             {
-                guests= guestsProcessing(command, guests);
+                var tokens = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var doubleOrRemove = tokens[0];
+                var criteria = tokens[1];
+                var parameters = tokens[2];
+
+                guestsPredicate = GetFunc(criteria);
+                var additionalGuests = new List<string>();
+
+                if (doubleOrRemove == "Double")
+                {
+                    additionalGuests = guests.Where(x => guestsPredicate(x, parameters)).ToList();
+
+                    foreach (var guest in additionalGuests)
+                    {
+                        var index = guests.IndexOf(guest);
+                        if (index == guests.Count - 1)
+                        {
+                            guests.Add(guest);
+                        }
+                        else
+                        {
+                            guests.Insert(index + 1, guest);
+                        }
+                    }
+                }
+                else
+                {   
+                    additionalGuests = guests.Where(x=>guestsPredicate(x,parameters)==false).ToList();
+                    guests = additionalGuests;
+                }
+                
                 command = Console.ReadLine();
             }
 
-            if (guests.Count > 0)
+            Console.WriteLine(guests.Any()? string.Join(", ", guests) + " are going to the party!"
+                : "Nobody is going to the party!");
+        }
+
+        public static Func<string,string,bool> GetFunc(string criteria)
+        {
+            if (criteria == "StartsWith")
             {
-                Console.WriteLine(string.Join(", ", guests) + " are going to the party!");
+                return (x,c) => x.StartsWith(c);
+            }
+            else if (criteria == "EndsWith")
+            {
+                return (x, c) => x.EndsWith(c);
+            }
+            else if (criteria == "Length")
+            {
+                return (x, c) => x.Length==int.Parse(c);
             }
             else
             {
-                Console.WriteLine("Nobody is going to the party!");
+                return null;
             }
-        }
-
-        public static List<string> DoubleOrRemove(string command, List<string> guests)
-        {
-            var token = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-            var doubleOrRemove = token[0];
-            var criteria = token[1];
-            var parameters = token[2];
-            var processedGuestList = new List<string>();
-
-            switch (doubleOrRemove)
-            {
-                case "Double":
-                    {
-                        if(criteria== "StartsWith")
-                        {
-                            foreach (var guest in guests)
-                            {
-                                processedGuestList.Add(guest);
-                                if (guest.StartsWith(parameters))
-                                {
-                                    processedGuestList.Add(guest);
-                                }
-                            }
-                        }
-                        else if(criteria == "EndsWith")
-                        {
-                            foreach (var guest in guests)
-                            {
-                                processedGuestList.Add(guest);
-                                if (guest.EndsWith(parameters))
-                                {
-                                    processedGuestList.Add(guest);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            var length = int.Parse(parameters);
-                            foreach (var guest in guests)
-                            {
-                                processedGuestList.Add(guest);
-                                if (guest.Length==length)
-                                {
-                                    processedGuestList.Add(guest);
-                                }
-                            }
-                        }
-                    }
-                    break;
-                case "Remove":
-                    {
-                        if (criteria == "StartsWith")
-                        {
-                            processedGuestList.AddRange(guests.Where(s => s.StartsWith(parameters)==false));
-                        }
-                        else if (criteria == "EndsWith")
-                        {
-                            processedGuestList.AddRange(guests.Where(s => s.EndsWith(parameters)==false));
-                        }
-                        else
-                        {
-                            var length = int.Parse(parameters);
-                            processedGuestList.AddRange(guests.Where(s => s.Length != length));
-                        }
-                    }
-                    break;
-            }
-
-            return processedGuestList;
         }
     }
 }
