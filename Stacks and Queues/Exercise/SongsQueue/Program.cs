e83c80c1478f44da9df07cfe3154b0e57d88c5e3
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-24 13:04:02 +0300

    stacks and queues

---
+++ Stacks and Queues/Exercise/SongsQueue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SongsQueue
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var songs = new Queue<string>(Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries));
+
+            while (songs.Any())
+            {
+                var command = Console.ReadLine().Split(new string[] { "Add " }, StringSplitOptions.RemoveEmptyEntries);
+
+                if(command[0] == "Play")
+                {
+                    songs.Dequeue();
+                }
+                else if (command[0] == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+                else
+                {
+                    var song = command[0];
+                    if (songs.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(song);
+                    }
+                }
+                
+            }
+
+            Console.WriteLine($"No more songs!");
+        }
+    }
+}
