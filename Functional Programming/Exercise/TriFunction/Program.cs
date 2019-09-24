1da3f29d6df0b9e40b7a8cea9503bbd64374acac
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-24 12:23:08 +0300

    functional programming

---
+++ Functional Programming/Exercise/TriFunction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TriFunction
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Console.ReadLine()
+                .Split()
+                .FirstOrDefault((x=>x.ToCharArray()
+                .Select(y=>(int)y).Sum()>=number)));
+        }
+    }
+}
