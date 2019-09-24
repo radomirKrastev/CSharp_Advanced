f2ba00d44c862fa38e2eb52e0ec22c839bc8f0fc
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-24 12:38:23 +0300

    functional programming nested funcs

---
+++ Functional Programming/Exercise/TriFunctionAnotherApproach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TriFunctionAnotherApproach
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            Func<string, char[]> getCharArr = x => x.ToCharArray();
+            Func<char, int> getCharValue= y => (int)y;
+            Func<string, bool> isValueValid = x=>getCharArr(x).Select(getCharValue).Sum() >= number;
+
+            Console.WriteLine(Console.ReadLine().Split().FirstOrDefault(isValueValid));
+        }
+    }
+}
