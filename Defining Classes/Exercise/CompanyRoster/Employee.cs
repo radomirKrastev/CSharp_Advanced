ae7e89d74fc537d0beed82d0c502c896b35d027d
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-26 13:53:19 +0300

    Company Roster

---
+++ Defining Classes/Exercise/CompanyRoster/Employee.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyRoster
+{
+    public class Employee
+    {
+        private string name;
+        private string position;
+        private string department;
+        private double salary;
+        private string email = "n/a";
+        private int age = -1;
+
+        public string Name
+        {
+            get=>this.name;
+            set=> this.name = value;
+        }
+
+        public string Position
+        {
+            get => this.position;
+            set => this.position = value;
+        }
+
+        public string Department
+        {
+            get => this.department;
+            set => this.department = value;
+        }
+
+        public double Salary
+        {
+            get => this.salary;
+            set => this.salary = value;
+        }
+
+        public string Email
+        {
+            get => this.email;
+            set => this.email = value;
+        }
+
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+
+        public Employee(string name, double salary, string position, string department)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Position = position;
+            this.Department = department;
+        }
+
+        public Employee(string name, double salary, string position, string department, int age)
+            : this(name, salary, position, department, "n/a", age)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Position = position;
+            this.Department = department;
+            this.Age = age;
+        }
+
+        public Employee(string name, double salary, string position, string department, string email)
+            :this(name,salary,position,department,email,-1)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Position = position;
+            this.Department = department;
+            this.Email = email;
+        }
+
+        public Employee(string name, double salary, string position,string department,string email,int age)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Position = position;
+            this.Department = department;
+            this.Email = email;
+            this.Age = age;
+        }
+    }
+}
---
+++ Defining Classes/Exercise/CompanyRoster/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompanyRoster
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var employeesCount = int.Parse(Console.ReadLine());
+            var employeesList = new List<Employee>();
+
+            for (int i = 0; i < employeesCount; i++)
+            {
+                var employeeInformation = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var name = employeeInformation[0];
+                var salary = double.Parse(employeeInformation[1]);
+                var position = employeeInformation[2];
+                var department = employeeInformation[3];
+
+                if(employeeInformation.Length == 4)
+                {
+                    employeesList.Add(new Employee(name, salary, position, department));
+                }
+                else if (employeeInformation.Length == 6)
+                {
+                    var email = employeeInformation[4];
+                    var age = int.Parse(employeeInformation[5]);
+
+                    employeesList.Add(new Employee(name, salary, position, department, email, age));
+                }
+                else if(employeeInformation.Length == 5)
+                {
+                    if(int.TryParse(employeeInformation[4], out int age))
+                    {
+                        employeesList.Add(new Employee(name, salary, position, department, age));
+                    }
+                    else
+                    {
+                        employeesList.Add(new Employee(name, salary, position, department, employeeInformation[4]));
+                    }
+                }
+            }
+
+            var departments = employeesList.GroupBy(x => x.Department);
+            var highestAverageSalary = 0d;
+            var highestPayingDepartment = string.Empty;
+            foreach (var department in departments)
+            {
+                var departmentEmployees = 0;
+                var averageSalary = 0d;
+                foreach (var salary in department.Select(x=>x.Salary))
+                {
+                    averageSalary += salary;
+                    departmentEmployees++;
+                }
+
+                averageSalary /= departmentEmployees;
+
+                if (highestAverageSalary < averageSalary)
+                {
+                    highestAverageSalary = averageSalary;
+                    highestPayingDepartment = department.Key;
+                }
+            }
+
+            Console.WriteLine($"Highest Average Salary: {highestPayingDepartment}");
+            foreach (var employee in employeesList.Where(x => x.Department == highestPayingDepartment)
+                .OrderByDescending(x=>x.Salary))
+            {
+                Console.WriteLine($"{employee.Name} {employee.Salary:F2} {employee.Email} {employee.Age}");
+            }
+        }
+    }
+}
