32c2ced4d8321c29fdb64b2fbe70ce6c84a3208e
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-26 10:07:32 +0300

    define class

---
+++ Defining Classes/Exercise/DefineClassPerson/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace DefineClassPerson
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get => this.name;
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get=>this.age;
+            set { this.age = value; }
+        }
+    }
+}
