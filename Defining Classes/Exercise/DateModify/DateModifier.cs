2597f9f2a877752d91573f79b9a10f62aaea7202
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-26 12:39:15 +0300

    DateModifier Class

---
+++ Defining Classes/Exercise/DateModify/DateModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModify
+{
+    public class DateModifier
+    {
+        public double GetDaysDifference(string firstDate, string secondDate)
+        {
+            var firstDateInfo = firstDate.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var secondDateInfo = secondDate.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var dateOne = new DateTime(int.Parse(firstDateInfo[0]), int.Parse(firstDateInfo[1]), int.Parse(firstDateInfo[2]));
+            var dateTwo = new DateTime(int.Parse(secondDateInfo[0]), int.Parse(secondDateInfo[1]), int.Parse(secondDateInfo[2]));
+
+            var timeSpan = dateOne - dateTwo;
+            double result = Math.Abs(timeSpan.TotalDays);
+            return result;
+        }
+    }
+}
---
+++ Defining Classes/Exercise/DateModify/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateModify
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var dateOne = Console.ReadLine();
+            var dateTwo = Console.ReadLine();
+
+            var dayModifier = new DateModifier();
+            var daysDifference = dayModifier.GetDaysDifference(dateOne, dateTwo);
+            Console.WriteLine(daysDifference);
+        }
+    }
+}
