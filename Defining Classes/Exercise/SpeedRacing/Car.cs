a53a3214b9d163537281990169ac74f97eb4fda2
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-26 15:18:08 +0300

    Speed Racing

---
+++ Defining Classes/Exercise/SpeedRacing/Car.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumption; //per km
+        private double distanceTraveled=0;
+
+        public string Model
+        {
+            get => this.model;
+            private set { this.model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get => this.fuelAmount;
+            private set { this.fuelAmount = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get => this.fuelConsumption;
+            private set { this.fuelConsumption = value; }
+        }
+
+        public double DistanceTraveled
+        {
+            get => this.distanceTraveled;
+            private set { this.distanceTraveled = value; }
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumption)
+        {
+            this.Model = model;
+            this.FuelAmount= fuelAmount;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public string Drive(double distance)
+        {
+            var result = string.Empty;
+            if (this.fuelAmount >= distance * this.fuelConsumption)
+            {
+                this.distanceTraveled += distance;
+                this.fuelAmount -= distance * this.fuelConsumption;
+            }
+            else
+            {
+                result = "Insufficient fuel for the drive";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Defining Classes/Exercise/SpeedRacing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedRacing
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numberOfCars = int.Parse(Console.ReadLine());
+            var cars = new List<Car>();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                var carInformation = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var model = carInformation[0];
+                var fuelAmount = double.Parse(carInformation[1]);
+                var fuelConsumption = double.Parse(carInformation[2]);
+
+                cars.Add(new Car(model, fuelAmount, fuelConsumption));
+            }
+
+            var command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                var tokens = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var model = tokens[1];
+                var distance = double.Parse(tokens[2]);
+
+                var carModel = cars.Find(x=>x.Model==model);
+                var result = carModel.Drive(distance);
+                if (result != string.Empty)
+                {
+                    Console.WriteLine(result);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.DistanceTraveled}");
+            }
+        }
+    }
+}
