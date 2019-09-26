41b15de1e27a507ef14820b4f5f408a056f9df81
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-26 11:24:07 +0300

    add another logic -Opinion Poll

--- Defining Classes/Exercise/DefineClassPerson/StartUp.cs
+++ Defining Classes/Exercise/DefineClassPerson/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DefiningClasses
 {
@@ -6,21 +8,37 @@ namespace DefiningClasses
     {
         public static void Main()
         {
-            var familyCount = int.Parse(Console.ReadLine());
-            var family = new Family();
+            var peopleCount = int.Parse(Console.ReadLine());
+            var pollParticipants = new List<Person>();
 
-            for (int i = 0; i < familyCount; i++)
+            for (int i = 0; i < peopleCount; i++)
             {
-                var memberInformation = Console.ReadLine().Split(new string [] {" "},StringSplitOptions.RemoveEmptyEntries);
-                var name = memberInformation[0];
-                var age = int.Parse(memberInformation[1]);
+                var personInformation = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var name = personInformation[0];
+                var age = int.Parse(personInformation[1]);
 
-                var member = new Person(name,age);
-                family.AddMember(member);
+                pollParticipants.Add(new Person(name, age));
             }
 
-            var oldestMember = family.GetOldestMember();
-            Console.WriteLine(oldestMember.Name+" "+oldestMember.Age);
+            foreach (var person in pollParticipants.Where(x=>x.Age>30).OrderBy(x=>x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+            //var familyCount = int.Parse(Console.ReadLine());
+            //var family = new Family();
+
+            //for (int i = 0; i < familyCount; i++)
+            //{
+            //    var memberInformation = Console.ReadLine().Split(new string [] {" "},StringSplitOptions.RemoveEmptyEntries);
+            //    var name = memberInformation[0];
+            //    var age = int.Parse(memberInformation[1]);
+
+            //    var member = new Person(name,age);
+            //    family.AddMember(member);
+            //}
+
+            //var oldestMember = family.GetOldestMember();
+            //Console.WriteLine(oldestMember.Name+" "+oldestMember.Age);
         }
     }
 }
