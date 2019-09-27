f423b6b046c99e6ace0656c495121751b24e242b
radomirKrastev <radomirkostantinov@gmail.com>
2019-09-27 09:05:23 +0300

    Rectangles Intersection

---
+++ Defining Classes/Exercise/RectangleIntersection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RectangleIntersection
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(new string[] { " " },StringSplitOptions.RemoveEmptyEntries);
+            var rectanglesCount = int.Parse(input[0]);
+            var intersectionsCount = int.Parse(input[1]);
+            var rectanglesList = new List<Rectangle>();
+
+            for (int i = 0; i < rectanglesCount; i++)
+            {
+                var rectangle = Console.ReadLine().Split(new string[] { " " },StringSplitOptions.RemoveEmptyEntries);
+                rectanglesList.Add(new Rectangle(rectangle[0]
+                    , int.Parse(rectangle[1])
+                    ,int.Parse(rectangle[2])
+                    , int.Parse(rectangle[3])
+                    , int.Parse(rectangle[4])));
+            }
+
+            for (int i = 0; i < intersectionsCount; i++)
+            {
+                var rectanglesIdentification = Console.ReadLine().Split(new string[] { " " },StringSplitOptions.RemoveEmptyEntries);
+                var rectangleOneId = rectanglesIdentification[0];
+                var rectangleTwoId = rectanglesIdentification[1];
+
+                Rectangle rectangleOne = rectanglesList.FirstOrDefault(x => x.Id == rectangleOneId);
+                Rectangle rectangleTwo = rectanglesList.FirstOrDefault(x => x.Id == rectangleTwoId);
+
+                if(rectangleOne.LeftCornerHorizontal<=rectangleTwo.Width || rectangleOne.LeftCornerVertical <= rectangleTwo.Height)
+                {
+                    Console.WriteLine("true");
+                }
+            }
+        }
+    }
+}
---
+++ Defining Classes/Exercise/RectangleIntersection/Rectangle.cs
@@ -0,0 +1,55 @@
+﻿namespace RectangleIntersection
+{
+    public class Rectangle
+    {
+        private string id;
+        private int width;
+        private int height;
+        private int leftCornerHorizontal;
+        private int leftCornerVertical;
+
+        public string Id
+        {
+            get => this.id;
+            set => this.id = value;
+        }
+
+        public int Width
+        {
+            get => this.width;
+            set => this.width = value;
+        }
+
+        public int Height
+        {
+            get => this.height;
+            set => this.height = value;
+        }
+
+        public int LeftCornerHorizontal
+        {
+            get => this.leftCornerHorizontal;
+            set => this.leftCornerHorizontal = value;
+        }
+
+        public int LeftCornerVertical
+        {
+            get => this.leftCornerVertical;
+            set => this.leftCornerVertical = value;
+        }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle (string id, int width, int height, int leftCornerHorizontal, int lefrCornerVertical)
+        {
+            this.Id = id;
+            this.Width = width;
+            this.Height = height;
+            this.LeftCornerHorizontal = leftCornerHorizontal;
+            this.LeftCornerVertical = leftCornerVertical;
+        }
+    }
+}
