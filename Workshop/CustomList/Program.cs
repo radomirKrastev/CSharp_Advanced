4990d4dceaa77085f6de77c6f826e318cf3dc01b
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-02 12:21:58 +0300

    Add Reverse method

--- Workshop/CustomList/Program.cs
+++ Workshop/CustomList/Program.cs
@@ -9,12 +9,12 @@ namespace CustomList
         {
             var customList = new CustomList<int>();
 
-            customList.Add(1);
-            customList.Add(3);
-            customList.Add(4);
-            customList.Add(5);
-            customList.Add(6);
-            customList.Add(7);
+            //customList.Add(1);
+            //customList.Add(3);
+            //customList.Add(4);
+            //customList.Add(5);
+            //customList.Add(6);
+            //customList.Add(7);
             //customList.Add(8);
             //customList.Add(9);
 
@@ -28,16 +28,11 @@ namespace CustomList
             //customList.Insert(5, 15);
             //Console.WriteLine(customList.Contains(8));
 
-            //var list = new List<int>() { 1, 2, 3 };
-
-            //var foundElement = list.Find(x=>x+1==2);
-            //Console.WriteLine(foundElement);
-
             //customList.Swap(7, 1);
 
-            var found= customList.Find(x => x + 2 == 10);
-            customList.Reverse();
-            Console.WriteLine(found);
+            //var found= customList.Find(x => x + 2 == 10);
+            //customList.Reverse();
+            //Console.WriteLine(found);
         }
     }
 }
