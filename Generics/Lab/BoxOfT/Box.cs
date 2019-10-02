2ac9bc65c62cd159e9c20d8ee9ee142f3987bd16
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-02 15:39:04 +0300

    create generic class Box

---
+++ Generics/Lab/BoxOfT/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> elements;
+        
+        public int Count { get; private set; }
+
+        public Box()
+        {
+            this.elements = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.elements.Add(element);
+            this.Count++;
+        }
+
+        public T Remove()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Box is empty!");
+            }
+
+            var result = this.elements[this.Count - 1];
+            this.elements.RemoveAt(this.Count - 1);
+            this.Count--;
+            return result;
+        }
+    }
+}
---
+++ Generics/Lab/BoxOfT/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var box = new Box<int>();
+            box.Add(5);
+            box.Add(6);
+            box.Add(7);
+
+            Console.WriteLine();
+            var count = box.Count;
+
+            box.Remove();
+            box.Remove();
+
+            count = box.Count;
+
+            Console.WriteLine();
+
+            box.Remove();
+            box.Remove();
+        }
+    }
+}
