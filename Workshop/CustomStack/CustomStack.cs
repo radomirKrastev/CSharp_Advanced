437f2728f467aa8c2f80a5d87aed8eb48d2c79a2
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-02 14:00:35 +0300

    Implement CustomStack with Push,Pop,Peak and ForEach methods

---
+++ Workshop/CustomStack/CustomStack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class CustomStack<T>
+    {
+        private int initialCapacity = 4;
+
+        private T[] elements;
+
+        public CustomStack()
+        {
+            this.elements = new T[initialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(T element)
+        {
+            if (this.Count == this.elements.Length)
+            {
+                Resize();
+            }
+
+            this.elements[this.Count++] = element;
+        }
+        
+        public T Pop()
+        {
+            ValidateIfListIsEmpty();
+
+            var element = this.elements[this.Count - 1];
+            this.elements[this.Count - 1] = default(T);
+            this.Count--;
+            return element;
+        }
+
+        public T Peak()
+        {
+            ValidateIfListIsEmpty();
+
+            var element = this.elements[this.Count - 1];
+            return element;
+        }
+
+        public void Foreach(Action<T> action)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                action(this.elements[i]);
+            }
+        }
+
+        private void Resize()
+        {
+            var newArray = new T[this.Count * 2];
+            Array.Copy(this.elements, newArray, this.Count);
+            this.elements = newArray;
+        }
+
+        private void ValidateIfListIsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty!");
+            }
+        }
+    }
+}
---
+++ Workshop/CustomStack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var customStack = new CustomStack<int>();
+
+            customStack.Push(1);
+            customStack.Push(2);
+            customStack.Push(3);
+            customStack.Push(4);
+            customStack.Push(5);
+            customStack.Push(6);
+            customStack.Push(7);
+            customStack.Push(8);
+
+
+            //var stack = new Stack<int>();
+            //var peak = stack.Peek();
+            //Console.WriteLine(peak);
+            var peak = customStack.Peak();
+            Console.WriteLine(peak);
+            Console.WriteLine();
+            customStack.Foreach(x => Console.Write(x+" "));
+        }
+    }
+}
