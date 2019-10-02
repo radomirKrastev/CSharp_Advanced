555b66180d02d5c23cdc9b9d48763cea6a047707
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-02 18:46:50 +0300

    GenericScale

---
+++ Generics/Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T leftElement;
+        private T rightElement;
+
+        public EqualityScale(T leftElement,T rightElement)
+        {
+            this.leftElement = leftElement;
+            this.rightElement = rightElement;
+        }
+
+        public bool AreEqual()
+        {
+            var result = leftElement.Equals(rightElement);
+            return result;
+        }
+    }
+}
---
+++ Generics/Lab/GenericScale/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var leftItem = 5;
+            var rightItem = 5;
+
+            var scale = new EqualityScale<int>(leftItem, rightItem);
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
