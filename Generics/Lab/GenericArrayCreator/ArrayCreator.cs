c12757ae7fd43d3d52f7e4d28570c7fb46de7492
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-02 17:26:34 +0300

    create generic class ArrayCreator

---
+++ Generics/Lab/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public T[] Create<T> (int length, T element)
+        {
+            var elements = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                elements[i] = element;
+            }
+
+            return elements;
+        }
+    }
+}
---
+++ Generics/Lab/GenericArrayCreator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var strings = new ArrayCreator<string>();
+
+            string[] stringsArr = new ArrayCreator<string>().Create(5, "Gosho");
+
+            Console.WriteLine(string.Join(" ",stringsArr));
+        }
+    }
+}
