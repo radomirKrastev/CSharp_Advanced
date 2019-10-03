903c27d0119367c18df9403b033a3d96fc12ce45
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-03 15:37:30 +0300

    Create custom Tuple class with three generics

---
+++ Generics/Exercise/Threeuple/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Threeuple
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var personInformation = Console.ReadLine().Split();
+            var name = personInformation[0] + " " + personInformation[1];
+            var adress = personInformation[2];
+            var town = personInformation[3];
+            var firstTuple = new Tuple<string, string,string>(name, adress,town);
+
+            var personalBeerInformation = Console.ReadLine().Split();
+            var secondName = personalBeerInformation[0];
+            var state = personalBeerInformation[2];
+            var beerTolerance = int.Parse(personalBeerInformation[1]);
+            var secondTuple = new Tuple<string, int,bool>(secondName, beerTolerance,state=="drunk"?true:false);
+
+            var bankInformation = Console.ReadLine().Split();
+            var accountName = bankInformation[0];
+            var balance = double.Parse(bankInformation[1]);
+            string bankName = bankInformation[2];
+            var thirdTuple = new Tuple<string, double,string>(accountName, balance,bankName);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ Generics/Exercise/Threeuple/Tuple.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Threeuple
+{
+    public class Tuple<T, K, X>
+    {
+        private T item1;
+        private K item2;
+        private X item3;
+
+        public Tuple(T itemOne, K itemTwo, X itemThree)
+        {
+            this.item1 = itemOne;
+            this.item2 = itemTwo;
+            this.item3 = itemThree;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.item1} -> {this.item2} -> {this.item3}";
+        }
+    }
+}
