935f66634b10abf90e01bd84c27cd55cf7b61840
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-03 11:26:29 +0300

    GenericBox (int) tested

---
+++ Generics/Exercise/GenericBoxOfInt/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfInt
+{
+    public class Box<T>
+    {
+        private T element;
+
+        public Box(T value)
+        {
+            this.element = value;
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine($"{element.GetType()}: {element}");
+        }
+    }
+}
---
+++ Generics/Exercise/GenericBoxOfInt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxOfInt
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var linesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                var data = int.Parse(Console.ReadLine());
+                var item = new Box<int>(data);
+                item.ToString();
+            }
+        }
+    }
+}
