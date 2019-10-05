2366fd6142ba93514654c364911913c61cad90eb
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-05 16:31:19 +0300

    implement IEnimerable interface using yield return

---
+++ Iterators and Comparators/Lab/IteratorsAndComparators/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Authors = authors;
+            this.Title = title;
+            this.Year = year;
+        }
+    }
+}
---
+++ Iterators and Comparators/Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books = new List<Book>();
+
+        public Library(params Book [] books)
+        {
+            this.books= new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            foreach (var book in this.books)
+            {
+                yield return book;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
