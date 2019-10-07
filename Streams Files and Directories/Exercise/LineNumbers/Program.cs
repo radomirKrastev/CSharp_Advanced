3ec81d991e5a15a9fdebedf5f058ddab5f423fc4
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-07 18:22:49 +0300

    using class File

---
+++ Streams Files and Directories/Exercise/LineNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            File.Create("output.txt").Close();
+            var allLines = File.ReadAllLines("text.txt");
+
+            for (int i = 0; i < allLines.Length; i++)            
+            {
+                var line = allLines[i];
+                var lettersCount = 0;
+                var punctuationCount = 0;
+
+                foreach (var c in line)
+                {
+                    if (Char.IsPunctuation(c))
+                    {
+                        punctuationCount++;
+                    }
+                    else if (Char.IsLetter(c))
+                    {
+                        lettersCount++;
+                    }
+                }
+
+                allLines[i] = $"Line {i + 1}: {line} ({lettersCount})({punctuationCount})";
+            }
+
+            File.AppendAllLines("output.txt",allLines);
+        }
+    }
+}
