3857dbbdc1fa8c98a3a34b15a76f8cc9f1b5eb3e
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-07 14:57:49 +0300

    Even Lines problem using StreamReader

---
+++ Streams Files and Directories/Exercise/EvenLine/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EvenLines
+{
+    public class ExtensionMethods
+    {
+        public static string ReplaceAndReverse(string text, string pattern)
+        {
+            var temporaryResult = Regex.Replace(text, pattern,"@");
+            var words = temporaryResult.Split(new string[] { " "},StringSplitOptions.RemoveEmptyEntries).Reverse();
+
+            return string.Join(" ",words);
+        }
+    }
+}
---
+++ Streams Files and Directories/Exercise/EvenLine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace EvenLines
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using (StreamReader reader = new StreamReader("text.txt"))
+            {
+                var lineCounter = 0;
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var pattern = @"[-!,.?]";
+
+                    if (lineCounter++ % 2 == 0)
+                    {
+                        Console.WriteLine(ExtensionMethods.ReplaceAndReverse(line,pattern));
+                    }
+                }
+            }
+        }
+    }
+}
