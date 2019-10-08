46314a4c1816cbe2ee8dd9997d24d8e9a347e983
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-08 21:47:02 +0300

    update

--- Streams Files and Directories/Exercise/ZipAndExtract/Program.cs
+++ Streams Files and Directories/Exercise/ZipAndExtract/Program.cs
@@ -10,7 +10,6 @@ namespace ZipAndExtract
             string startPath = @"start";
             string zipPath = @"result.zip";
             string extractPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\extractFolder";
-
             ZipFile.CreateFromDirectory(startPath, zipPath);
             ZipFile.ExtractToDirectory(zipPath,extractPath);
         }
