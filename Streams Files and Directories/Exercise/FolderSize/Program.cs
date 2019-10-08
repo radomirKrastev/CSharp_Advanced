3383b66bdbe14b7afedf91e90d2e0d2b54304887
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-08 21:38:14 +0300

    traverse through directory recursively and get files size

---
+++ Streams Files and Directories/Exercise/FolderSize/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace FolderSize
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory()+ "\\New folder";
+            
+            var filesSize = GetFiles(currentDirectory);
+
+            Console.WriteLine(filesSize);
+        }
+
+        private static long GetFiles(string directory)
+        {
+            var totalSize = 0L;
+            var subDirectories = Directory.GetDirectories(directory);
+
+            foreach (var subDirectory in subDirectories)
+            {
+                totalSize+= GetFiles(subDirectory);
+            }
+
+            var files = Directory.GetFiles(directory);
+
+            foreach (var file in files)
+            {
+                totalSize+= new FileInfo(file).Length;
+            }
+
+            return totalSize;
+        }
+    }
+}
