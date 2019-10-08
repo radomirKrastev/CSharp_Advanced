97941d43aa6d237a4e280b51a5b7ed2d89f316fd
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-08 11:03:48 +0300

    copy binary file

---
+++ Streams Files and Directories/Exercise/CopyBinaryFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace CopyBinaryFile
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using (FileStream writer = new FileStream("output.png", FileMode.OpenOrCreate))
+            {
+                using (FileStream reader = new FileStream("copyMe.png", FileMode.Open))
+                {
+                    while (true)
+                    {
+                        var buffer = new byte[4096];
+                        var read = reader.Read(buffer, 0, buffer.Length);
+                        if (read == 0)
+                        {
+                            break;
+                        }
+                        else if(read<buffer.Length)
+                        {
+                            writer.Write(buffer,0,read);
+                            break;
+                        }
+                        else
+                        {
+                            writer.Write(buffer, 0, buffer.Length);
+                        }
+                    }
+                    
+                }
+            }            
+        }
+    }
+}
