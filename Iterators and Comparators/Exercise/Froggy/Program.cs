93eea547bb1d9238495c3241bdf41af967b2ebce
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-09 18:13:38 +0300

    iterators class Lake

---
+++ Iterators and Comparators/Exercise/Froggy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var lake = new Lake(Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
