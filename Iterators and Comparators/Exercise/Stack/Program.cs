daeaca7b8a28ab9aa6da57c550235b914cc5decd
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-09 17:47:53 +0300

    class stack

---
+++ Iterators and Comparators/Exercise/Stack/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var command = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            while (command != "END")
+            {
+                if (command.Split(" ")[0] == "Push")
+                {
+                    var elements = command.Substring(5)
+                        .Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToList();
+
+                    stack.Push(elements);
+
+                }
+                else if (command == "Pop")
+                {
+                    stack.Pop();
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ Iterators and Comparators/Exercise/Stack/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> list;
+
+        public Stack()
+        {
+            this.list = new List<T>();
+        }
+
+        public void Push(List<T> elements)
+        {
+            this.list.AddRange(elements);
+        }
+
+        public T Pop()
+        {
+            if (this.list.Count == 0)
+            {
+                throw new InvalidOperationException("No elements!");
+            }
+
+            var element = this.list[this.list.Count - 1];
+            this.list.RemoveAt(this.list.Count - 1);
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.list.Count-1; i >= 0; i--)
+            {
+                yield return this.list[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
