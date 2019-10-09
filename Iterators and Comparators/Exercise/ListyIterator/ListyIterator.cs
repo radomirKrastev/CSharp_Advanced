bee0abddf9db5129aff771d1c495c42204fc0af4
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-09 15:32:25 +0300

    Iterators

---
+++ Iterators and Comparators/Exercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+        private int index = 0;
+
+        public ListyIterator(List<T> collection)
+        {
+            this.collection = collection;
+        }
+
+        public bool Move()
+        {
+            if (this.index + 1 < this.collection.Count)
+            {
+                this.index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (this.index + 1 < this.collection.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Print()
+        {
+            if (this.collection.Count > 0)
+            {
+                Console.WriteLine(this.collection[index]);
+                return;
+            }
+
+            throw new InvalidOperationException("Invalid Operation!");
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in collection)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ Iterators and Comparators/Exercise/ListyIterator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+    public class Program 
+    {
+        public static void Main()
+        {
+            var command = Console.ReadLine();
+            var listyOperator = new ListyIterator<string>(new List<string>());
+
+            while (command != "END")
+            {
+                var tokens = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                if (tokens[0] == "Create")
+                {
+                    if (tokens.Length>1)
+                    {
+                        var listTokens = tokens.ToList();
+                        listTokens.RemoveAt(0);
+                        listyOperator = new ListyIterator<string>(new List<string>(listTokens));
+                    }
+                }
+                else if (command == "Move")
+                {
+                    Console.WriteLine(listyOperator.Move());
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(listyOperator.HasNext());                    
+                }
+                else if (command == "Print")
+                {
+                    listyOperator.Print();
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
