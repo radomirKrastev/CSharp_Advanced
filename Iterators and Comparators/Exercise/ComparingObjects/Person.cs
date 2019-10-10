3422eedb78a4086d5b62cd5e67835da4608fcaae
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-10 14:31:30 +0300

    update

---
+++ Iterators and Comparators/Exercise/ComparingObjects/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public string name;
+        public int age;
+        public string town;
+
+        public Person(string name, int age, string town)
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+
+        public int CompareTo(Person other)
+        {
+            var nameResult = this.name.CompareTo(other.name);
+
+            if (nameResult == 0)
+            {
+                var ageResult = this.age.CompareTo(other.age);
+
+                if (ageResult == 0)
+                {
+                    return this.town.CompareTo(other.town);
+                }
+
+                return ageResult;
+            }
+
+            return nameResult;
+        }
+    }
+}
---
+++ Iterators and Comparators/Exercise/ComparingObjects/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var people = new List<Person>();
+
+            var command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                var tokens = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                people.Add(new Person(tokens[0], int.Parse(tokens[1]), tokens[2]));
+                command = Console.ReadLine();
+            }
+
+            var number = int.Parse(Console.ReadLine());
+            var keyPerson = people[number - 1];
+            var equalPeople = 0;
+            var differentPeople = 0;
+
+            foreach (var person in people)
+            {
+                if (person.CompareTo(keyPerson) == 0)
+                {
+                    equalPeople++;
+                }
+                else
+                {
+                    differentPeople++;
+                }
+            }
+
+            if (equalPeople-1 == 0)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equalPeople} {differentPeople} {people.Count}");
+            }
+        }
+    }
+}
