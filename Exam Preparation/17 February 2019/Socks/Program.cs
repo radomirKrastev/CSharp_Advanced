00fa69808ffb56c37f96273766569977e32f2a43
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-21 14:34:02 +0300

    exam preparation first problem

---
+++ Exam Preparation/17 February 2019/Socks/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Socks
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var leftSocks = new Stack<int>(Console.ReadLine()
+                .Split(new string[] { " "},StringSplitOptions
+                .RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            var rightSocks = new Queue<int>(Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions
+                .RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            var pairs = new List<int>();
+
+            while (leftSocks.Any())
+            {
+                var leftSock = leftSocks.Pop();
+                var rightSock = rightSocks.Peek();
+
+                if (leftSock > rightSock)
+                {
+                    pairs.Add(leftSock + rightSock);
+                    rightSocks.Dequeue();
+                }
+                else if (rightSock > leftSock)
+                {
+                    continue;
+                }
+                else
+                {
+                    rightSocks.Dequeue();
+                    leftSocks.Push(++leftSock);
+                }
+            }
+
+            Console.WriteLine (pairs.OrderByDescending(x => x).First());
+            Console.WriteLine(string.Join(" ",pairs));
+        }
+    }
+}
