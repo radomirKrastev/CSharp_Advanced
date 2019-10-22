f0b17310c6d3e24a595899149ab40885c55d52f3
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-22 12:37:17 +0300

    exam preparation first problem

---
+++ Exam Preparation/24 February 2019/ClubParty/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClubParty
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var hallCapacity = int.Parse(Console.ReadLine());
+
+            var halls = new Queue<char>();
+
+            var hallsAndReservations = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            hallsAndReservations.Reverse();
+
+            var hallGuests = new List<int>();
+
+            foreach (var item in hallsAndReservations)
+            {
+                if (int.TryParse(item, out int guests))
+                {
+                    if (!halls.Any())
+                    {
+                        continue;
+                    }
+
+                    if (hallGuests.Sum() + guests > hallCapacity)
+                    {
+                        Console.WriteLine($"{halls.Dequeue()} -> {string.Join(", ", hallGuests)}");
+                        hallGuests.Clear();
+                    }
+
+                    if (halls.Any())
+                    {
+                        hallGuests.Add(guests);
+                    }
+                }
+                else if (char.TryParse(item, out char result))
+                {
+                    halls.Enqueue(result);
+                }
+            }
+        }
+    }
+}
