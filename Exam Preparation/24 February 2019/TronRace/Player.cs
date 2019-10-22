362ae797ad653fecf99323d2753f2ef916ca64e1
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-22 14:06:15 +0300

    exam preparation second problem

---
+++ Exam Preparation/24 February 2019/TronRace/Player.cs
@@ -0,0 +1,91 @@
+﻿namespace TronRace
+{
+    public class Player
+    {
+        public Player(char trail)
+        {
+            this.IsAlive = true;
+            this.Trail = trail;
+            this.Position = new int[2];
+        }
+
+        public int[] Position { get; set; }
+        public bool IsAlive { get; private set; }
+        public char Trail { get; set; }
+
+        public void Move(char[,] matrix,string direction)
+        {
+            var destinateRow = this.Position[0];
+            var destinateCol = this.Position[1];
+
+            if (direction == "up")
+            {
+                destinateRow -= 1;
+
+                if (!InBorderValidator(matrix, destinateRow, destinateCol))
+                {
+                    destinateRow = matrix.GetUpperBound(0);
+                }                
+            }
+            else if(direction == "down")
+            {
+                destinateRow += 1;
+
+                if (!InBorderValidator(matrix, destinateRow, destinateCol))
+                {
+                    destinateRow = 0;
+                }
+            }
+            else if(direction == "left")
+            {
+                destinateCol -= 1;
+
+                if (!InBorderValidator(matrix, destinateRow, destinateCol))
+                {
+                    destinateCol = matrix.GetUpperBound(1);
+                }
+            }
+            else if(direction == "right")
+            {
+                destinateCol += 1;
+
+                if (!InBorderValidator(matrix, destinateRow, destinateCol))
+                {
+                    destinateCol = 0;
+                }
+            }
+
+            AdjustPosition(destinateRow, destinateCol);
+            MoveOrDie(matrix);
+        }
+
+        private void MoveOrDie(char[,] matrix)
+        {
+            if (matrix[this.Position[0], this.Position[1]] != '*' && matrix[this.Position[0], this.Position[1]] != this.Trail)
+            {
+                matrix[this.Position[0], this.Position[1]] = 'x';
+                this.IsAlive = false;
+            }
+            else
+            {
+                matrix[this.Position[0], this.Position[1]] = this.Trail;
+            }
+        }
+
+        private void LeaveTrail(char[,] matrix)
+        {
+            matrix[this.Position[0], this.Position[1]] = this.Trail;
+        }
+
+        private void AdjustPosition(int destinateRow, int destinateCol)
+        {
+            this.Position[0] = destinateRow;
+            this.Position[1] = destinateCol;
+        }
+
+        private bool InBorderValidator(char [,] matrix, int row, int col)
+        {
+            return row >= 0 && col >= 0 && row <= matrix.GetUpperBound(0) && col <= matrix.GetUpperBound(1);
+        }
+    }
+}
---
+++ Exam Preparation/24 February 2019/TronRace/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace TronRace
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var dimension = int.Parse(Console.ReadLine());
+            var rows = dimension;
+            var cols = dimension;
+
+            var firstPlayer = new Player('f');
+            var secondPlayer = new Player('s');
+            var matrix = InitializeMatrix(rows, cols, firstPlayer, secondPlayer);
+
+            while (firstPlayer.IsAlive && secondPlayer.IsAlive)
+            {
+                var commands = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var firstPlayerCommand = commands[0];
+                var secondPlayerCommand = commands[1];
+
+                firstPlayer.Move(matrix, firstPlayerCommand);
+
+                if (firstPlayer.IsAlive == false)
+                {
+                    break;
+                }
+
+                secondPlayer.Move(matrix, secondPlayerCommand);
+
+                if (secondPlayer.IsAlive == false)
+                {
+                    break;
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+
+                if (row < rows - 1)
+                {
+                    Console.WriteLine();
+                }                
+            }
+        }
+
+        private static char[,] InitializeMatrix(int rows, int cols, Player firstPlayer, Player secondPlayer)
+        {
+            var matrix = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var line = Console.ReadLine();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = line[col];
+
+                    if (matrix[row, col] == 'f')
+                    {
+                        firstPlayer.Position[0] = row;
+                        firstPlayer.Position[1] = col;
+                    }
+                    else if (matrix[row, col] == 's')
+                    {
+                        secondPlayer.Position[0] = row;
+                        secondPlayer.Position[1] = col;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
