aafe184460bdee14b7f98dbd9987e71df2723ab1
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-23 08:59:55 +0300

    exam preparation second problem

---
+++ Exam Preparation/16 April 2019/AbductionOfHelen/Paris.cs
@@ -0,0 +1,92 @@
+﻿namespace AbductionOfHelen
+{
+    public class Paris
+    {
+        private int energy;
+
+        public Paris(int energy)
+        {
+            this.energy = energy;
+            this.IsAlive = this.energy > 0;
+            this.SavedHelen = false;
+            this.Position = new int[2];
+        }
+
+        public int Energy => this.energy;
+        public bool IsAlive { get; private set; }
+        public bool SavedHelen { get; private set; }
+        public int[] Position { get; set; }
+
+        public void Move(char[][] matrix, string direction)
+        {
+            var destinedRow = this.Position[0];
+            var destinedCol = this.Position[1];
+
+            if (direction == "up")
+            {
+                destinedRow -= 1;
+            }
+            else if (direction == "down")
+            {
+                destinedRow += 1;
+            }
+            else if (direction == "left")
+            {
+                destinedCol -= 1;
+            }
+            else if (direction == "right")
+            {
+                destinedCol += 1;
+            }
+
+            this.energy -= 1;
+
+            if (ValidatePosition(matrix, destinedRow, destinedCol))
+            {
+                MovePosition(matrix, destinedRow, destinedCol);
+                AdjustField(matrix);
+            }
+
+            if (this.energy <= 0 && this.SavedHelen == false)
+            {
+                this.IsAlive = false;
+                matrix[this.Position[0]][this.Position[1]] = 'X';
+            }
+        }
+
+        private void AdjustField(char[][] matrix)
+        {
+            if (matrix[this.Position[0]][this.Position[1]] == 'S')
+            {
+                FightEnemy(matrix);
+            }
+            else if (matrix[this.Position[0]][this.Position[1]] == 'H')
+            {
+                matrix[this.Position[0]][this.Position[1]] = '-';
+                this.SavedHelen = true;
+            }
+            else if (matrix[this.Position[0]][this.Position[1]] == '-')
+            {
+                matrix[this.Position[0]][this.Position[1]] = 'P';
+            }
+        }
+
+        private void MovePosition(char[][] matrix, int destinedRow, int destinedCol)
+        {
+            matrix[this.Position[0]][this.Position[1]] = '-';
+            this.Position[0] = destinedRow;
+            this.Position[1] = destinedCol;
+        }
+
+        private void FightEnemy(char[][] matrix)
+        {
+            this.energy -= 2;
+            matrix[this.Position[0]][this.Position[1]] = 'P';
+        }
+
+        private bool ValidatePosition(char[][] matrix, int row, int col)
+        {
+            return row >= 0 && col >= 0 && row <= matrix.GetUpperBound(0) && col < matrix[row].Length;
+        }
+    }
+}
---
+++ Exam Preparation/16 April 2019/AbductionOfHelen/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace AbductionOfHelen
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var initialEnergy = int.Parse(Console.ReadLine());
+            var rows = int.Parse(Console.ReadLine());;
+
+            var paris = new Paris(initialEnergy);
+            var matrix = InitializeMatrix(rows, paris);
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (matrix[row][col] == 'P')
+                    {
+                        paris.Position[0] = row;
+                        paris.Position[1] = col;
+                    }
+                }
+            }
+
+            while (paris.IsAlive == true && paris.SavedHelen == false)
+            {
+                var command = Console.ReadLine()
+                    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                var direction = command[0];
+                var spartanRow = int.Parse(command[1]);
+                var spartanCol = int.Parse(command[2]);
+
+                matrix[spartanRow][spartanCol] = 'S';
+                paris.Move(matrix, direction);
+            }
+
+            if (paris.SavedHelen == true)
+            {
+                Console.WriteLine($"Paris has successfully abducted Helen! Energy left: {paris.Energy}");
+            }
+            else
+            {
+                Console.WriteLine($"Paris died at {paris.Position[0]};{paris.Position[1]}.");
+            }
+
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+
+        private static char[][] InitializeMatrix(int rows, Paris paris)
+        {
+            var matrix = new char[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var line = Console.ReadLine().ToCharArray();
+
+                matrix[row] = line;
+            }
+
+            return matrix;
+        }
+    }
+}
