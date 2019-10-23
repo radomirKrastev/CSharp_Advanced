e260a488223be688674fbea74c1d7dd7de4445c3
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-23 17:58:24 +0300

    exam preparation third problem

---
+++ Exam Preparation/23 October 2019/HealthyHeaven/Restaurant.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HealthyHeaven
+{
+    public class Restaurant
+    {
+        private List<Salad> data;
+
+        public Restaurant(string name)
+        {
+            this.Name = name;
+            this.data = new List<Salad>();
+        }
+
+        public string Name { get; set; }
+
+        public void Add(Salad salad)
+        {
+            this.data.Add(salad);
+        }
+
+        public Salad GetHealthiestSalad()
+        {
+            var salad = this.data.OrderBy(x => x.TotalCalories).First();
+
+            return salad;
+        }
+
+        public bool Buy (string name)
+        {
+            var salad = this.data.Where(x => x.Name == name).FirstOrDefault();
+            
+            if (salad == null)
+            {
+                return false;
+            }
+
+            this.data.Remove(salad);
+            return true;
+        }
+
+        public string GenerateMenu()
+        {
+            var output = new StringBuilder();
+
+            output.AppendLine($"{this.Name} have {this.data.Count} salads:");
+            this.data.ForEach(x => output.AppendLine($"{x}"));
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exam Preparation/23 October 2019/HealthyHeaven/Salad.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace HealthyHeaven
+{
+    public class Salad
+    {
+        private List<Vegetable> products;
+
+        public Salad(string name)
+        {
+            this.Name = name;
+            this.products = new List<Vegetable>();
+        }
+
+        public string Name { get; set; }
+        public int TotalCalories => GetTotalCalories();
+
+        public int GetProductCount()
+        {
+            return this.products.Count;
+        }
+
+        public int GetTotalCalories()
+        {
+            var calories = 0;
+
+            this.products.ForEach(x=> calories+=x.Calories);
+
+            return calories;
+        }
+
+        public void Add(Vegetable product)
+        {
+            this.products.Add(product);
+        }
+
+        public override string ToString()
+        {
+            var output = new StringBuilder();
+
+            output.AppendLine($"* Salad {this.Name} is {GetTotalCalories()} calories and have {this.products.Count} products:");
+            this.products.ForEach(x => output.AppendLine($"{x}"));
+
+            return output.ToString().Trim();
+        }
+    }
+}
