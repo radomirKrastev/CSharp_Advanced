c3ac597346958df8730a479ab68a8b9a3bfe6eaa
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-23 11:27:58 +0300

    exam preparation first problem

---
+++ Exam Preparation/13 August 2019/SummerCocktails/Cocktails.cs
@@ -0,0 +1,10 @@
+﻿namespace SummerCocktails
+{
+    public enum Cocktails
+    {
+         Mimosa = 150,
+         Daiquiri = 250,
+         Sunshine = 300,
+         Mojito = 400
+    }
+}
---
+++ Exam Preparation/13 August 2019/SummerCocktails/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SummerCocktails
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var ingredients = new Queue<int>(Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            var ingredientsFreshness = new Stack<int>(Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            var cocktails = new Dictionary<string, int>();
+
+            while(ingredients.Any() && ingredientsFreshness.Any())
+            {
+                var ingredient = ingredients.Dequeue();
+
+                if (ingredient == 0)
+                {
+                    continue;
+                }
+
+                var freshness = ingredientsFreshness.Pop();
+                var product = ingredient * freshness;
+
+                PrepareCocktail(product, cocktails, ingredients, ingredient);
+            }
+
+            if(GetTaskComplete(cocktails))
+            {
+                Console.WriteLine("It's party time! The cocktails are ready!");
+            }
+            else
+            {
+                Console.WriteLine("What a pity! You didn't manage to prepare all cocktails.");
+            }
+
+            if (ingredients.Any())
+            {
+                Console.WriteLine($"Ingredients left: {ingredients.Sum()}");
+            }
+
+            foreach (var cocktail in cocktails.Where(x=>x.Value>0).OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($" # {cocktail.Key} --> {cocktail.Value}");
+            }
+        }
+
+        private static void PrepareCocktail(int product, Dictionary<string,int> cocktails, Queue<int> ingredients, int ingredient)
+        {
+            if (product == (int)Cocktails.Mimosa)
+            {
+                var cocktail = Cocktails.Mimosa.ToString();
+
+                if (!cocktails.ContainsKey(cocktail))
+                {
+                    cocktails.Add(cocktail, 0);
+                }
+
+                cocktails[cocktail]++;
+            }
+            else if (product == (int)Cocktails.Daiquiri)
+            {
+                var cocktail = Cocktails.Daiquiri.ToString();
+
+                if (!cocktails.ContainsKey(cocktail))
+                {
+                    cocktails.Add(cocktail, 0);
+                }
+
+                cocktails[cocktail]++;
+            }
+            else if (product == (int)Cocktails.Sunshine)
+            {
+                var cocktail = Cocktails.Sunshine.ToString();
+
+                if (!cocktails.ContainsKey(cocktail))
+                {
+                    cocktails.Add(cocktail, 0);
+                }
+
+                cocktails[cocktail]++;
+            }
+            else if (product == (int)Cocktails.Mojito)
+            {
+                var cocktail = Cocktails.Mojito.ToString();
+
+                if (!cocktails.ContainsKey(cocktail))
+                {
+                    cocktails.Add(cocktail, 0);
+                }
+
+                cocktails[cocktail]++;
+            }
+            else
+            {
+                ingredient += 5;
+                ingredients.Enqueue(ingredient);
+            }
+        }
+
+        private static bool GetTaskComplete(Dictionary<string,int> cocktails)
+        {
+            return cocktails.Count == 4;
+        }
+    }
+}
