00ec23b6fb41ff75b9bc9e9a715e0675930826b5
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-27 16:52:27 +0200

    second problem from the exam

---
+++ Exam/BookWorm/Player.cs
@@ -0,0 +1,71 @@
+﻿namespace BookWorm
+{
+    public class Player
+    {
+        public Player(string message)
+        {
+            this.Position = new int[2];
+            this.Message = message;
+        }
+
+        public int[] Position { get; set; }
+        public string Message { get; private set; }
+
+        public void Move(char [,] matrix, string direction)
+        {
+            var destinateRow = this.Position[0];
+            var destinateCol = this.Position[1];
+
+            if (direction == "up")
+            {
+                destinateRow--;                
+            }
+            else if (direction == "down")
+            {
+                destinateRow++;
+            }
+            else if (direction == "left")
+            {
+                destinateCol--;
+            }
+            else if (direction == "right")
+            {
+                destinateCol++;
+            }
+
+            if (!ValidatePosition(destinateRow, destinateCol, matrix.GetLength(0)))
+            {
+                if (this.Message.Length > 0)
+                {
+                    if (this.Message.Length == 1)
+                    {
+                        this.Message = string.Empty;
+                    }
+                    else
+                    {
+                        this.Message = this.Message.Substring(0, this.Message.Length - 1);
+                    }
+                }
+            }
+            else
+            {
+                matrix[this.Position[0], this.Position[1]] = '-';
+
+                if(char.IsLetter(matrix[destinateRow, destinateCol]))
+                {
+                    this.Message += matrix[destinateRow, destinateCol];
+                }
+                
+                this.Position[0] = destinateRow;
+                this.Position[1] = destinateCol;
+
+                matrix[this.Position[0], this.Position[1]] = 'P';
+            }
+        }
+
+        private bool ValidatePosition(int row, int col, int size)
+        {
+            return row >= 0 && col >= 0 && row < size && col < size;
+        }
+    }
+}
---
+++ Exam/BookWorm/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace BookWorm
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var initialString = Console.ReadLine();
+            var rows = int.Parse(Console.ReadLine());
+
+            var player = new Player(initialString);
+
+            var cols = rows;
+            var matrix = InitializeMatrix(player, rows);
+
+            var command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                player.Move(matrix,command);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(player.Message);
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+
+                if (row != rows - 1)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private static char[,] InitializeMatrix(Player player,int size)
+        {
+            var matrix = new char[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                var line = Console.ReadLine();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = line[col];
+
+                    if (matrix[row, col] == 'P')
+                    {
+                        player.Position[0] = row;
+                        player.Position[1] = col;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
