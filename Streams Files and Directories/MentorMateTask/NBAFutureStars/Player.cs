7d8c35caaa48faa100374ef168287f235cced96b
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-04 11:09:20 +0200

    add task

---
+++ Streams Files and Directories/MentorMateTask/NBAFutureStars/Player.cs
@@ -0,0 +1,26 @@
+﻿namespace NBAFutureStars
+{
+    public class Player
+    {
+        private string name;
+        private int playerSince;
+        private string position;
+        private double rating;
+
+        public Player(string name, int playerSince, string position, double rating)
+        {
+            this.name = name;
+            this.playerSince = playerSince;
+            this.position = position;
+            this.rating = rating;
+        }
+
+        public string Name => this.name;
+
+        public int PlayerSince => this.playerSince;
+
+        public string Position => this.position;
+
+        public double Rating => this.rating;
+    }
+}
